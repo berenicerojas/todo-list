fcb0e6bec7d9d7726252c868466c493581e5060e
berenicerojas <berenicerojas.nyc@gmail.com>
2025-11-18 20:32:51 -0500

    feat: complete TodoForm and TodoList component isolation

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import './App.css'
+import TodoForm from './TodoForm'
+import './TodoForm'
+import TodoList from './TodoList'
+import './TodoList'
 
 function App() {
-  const todos = [
-    {id: 1, title: "review resources"},
-    {id: 2, title: "take notes"},
-    {id: 3, title: "code out app"},
-  ]
-
   return (
     <div>
       <h1>My Todos</h1>
-       <ul>
-            {todos.map(todo => <li key={todo.id}>{todo.title} :) </li>)}
-        </ul>
+      <TodoForm/> 
+      <TodoList/>
     </div>
   )
 }
