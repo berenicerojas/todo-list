de6e062043cc78bfcf0967d921c744338a6b99da
berenicerojas <berenicerojas.nyc@gmail.com>
2025-11-29 14:25:24 -0500

    feat: completed week 04 state and props

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './App.css'
 import TodoForm from './TodoForm'
 import './TodoForm'
@@ -5,10 +6,13 @@ import TodoList from './TodoList'
 import './TodoList'
 
 function App() {
+  const[newTodo,setNewTodo]= useState('Visible text')
+
   return (
     <div>
       <h1>My Todos</h1>
       <TodoForm/> 
+      <p>{newTodo}</p>
       <TodoList/>
     </div>
   )
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,15 +1,21 @@
-
+import ToDoListItem from "./TodoListItem";
 function TodoList(){
     const todos = [
     {id: 1, title: "review resources"},
     {id: 2, title: "take notes"},
     {id: 3, title: "code out app"},
-  ]
+    ]
  
     return(
-    <ul>
-        {todos.map(todo => <li key={todo.id}>{todo.title}</li>)} 
-    </ul>);
+      <ul>
+        {todos.map(todo =>(
+          <ToDoListItem 
+            todo={todo}
+            key={todo.id}
+          />
+        ))}
+    </ul>
+  );
 }
 
-export default TodoList
+export default TodoList;
